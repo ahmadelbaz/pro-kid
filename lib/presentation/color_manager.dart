65a9df2e79021d3e9f35b365bbe45a80d29dc26d
ahmadelbaz <ahmeed.elbaz@gmail.com>
2023-04-28 17:11:54 +0300

    Created ColorManager in presentation layer and added needed colors in it

---
+++ lib/presentation/color_manager.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class ColorManager {
+  static Color primary = const Color(0xff4B0082);
+  static Color darkGrey = const Color(0xff525252);
+  static Color grey = const Color(0xff737477);
+  static Color lightGrey = const Color(0xff9e9e9e);
+}
