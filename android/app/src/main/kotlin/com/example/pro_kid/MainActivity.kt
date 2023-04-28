1c7655b6b1a317d2a9f70efdffd7e55ed9500f8c
ahmadelbaz <ahmeed.elbaz@gmail.com>
2023-04-28 12:21:29 +0300

    Created the project, add layers of Clean architecture and applied singletone design patter to MyApp Class

---
+++ android/app/src/main/kotlin/com/example/pro_kid/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pro_kid
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/app/app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatefulWidget {
+  const MyApp._();
+  static const MyApp _instance = MyApp._();
+  factory MyApp.getInstance() => _instance;
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
